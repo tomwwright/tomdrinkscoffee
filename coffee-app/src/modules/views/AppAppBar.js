5d61618b2c4004c9dc12e3ba4b7134f6f7c06361
Tom Wright <tom.suffern.wright@gmail.com>
2020-06-12 21:41:20 +1000

    Update links and title in app bar

--- coffee-app/src/modules/views/AppAppBar.js
+++ coffee-app/src/modules/views/AppAppBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import clsx from "clsx";
 import { withStyles } from "@material-ui/core/styles";
 import Link from "@material-ui/core/Link";
 import AppBar from "../components/AppBar";
@@ -43,15 +42,12 @@ function AppAppBar(props) {
       <AppBar position="fixed">
         <Toolbar className={classes.toolbar}>
           <div className={classes.left} />
-          <Link variant="h6" underline="none" color="inherit" className={classes.title} href="/premium-themes/onepirate/">
-            {"onepirate"}
+          <Link variant="h6" underline="none" color="inherit" className={classes.title} href="/">
+            {"TOMDRINKSCOFFEE."}
           </Link>
           <div className={classes.right}>
-            <Link color="inherit" variant="h6" underline="none" className={classes.rightLink} href="/premium-themes/onepirate/sign-in/">
-              {"Sign In"}
-            </Link>
-            <Link variant="h6" underline="none" className={clsx(classes.rightLink, classes.linkSecondary)} href="/premium-themes/onepirate/sign-up/">
-              {"Sign Up"}
+            <Link color="inherit" variant="h6" underline="none" className={classes.rightLink} href="https://github.com/tomwwright/tomdrinkscoffee">
+              {"GITHUB."}
             </Link>
           </div>
         </Toolbar>
